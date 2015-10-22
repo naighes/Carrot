b2bb2d113efc4b130545c16474b9122e78893d52
Nicola Baldi <nic.baldi@gmail.com>
2015-10-22 17:32:26 +0200

    exchange are now non-duurable by default.

--- src/Carrot.Tests/AmqpEntities.cs
+++ src/Carrot.Tests/AmqpEntities.cs
@@ -12,6 +12,17 @@ namespace Carrot.Tests
             var exchange = Exchange.Direct(name);
             Assert.Equal(name, exchange.Name);
             Assert.Equal("direct", exchange.Type);
+            Assert.Equal(false, exchange.Durable);
+        }
+
+        [Fact]
+        public void BuildingDurableDirectExchange()
+        {
+            const String name = "one_exchange";
+            var exchange = Exchange.DurableDirect(name);
+            Assert.Equal(name, exchange.Name);
+            Assert.Equal("direct", exchange.Type);
+            Assert.Equal(true, exchange.Durable);
         }
 
         [Fact]
@@ -21,6 +32,17 @@ namespace Carrot.Tests
             var exchange = Exchange.Fanout(name);
             Assert.Equal(name, exchange.Name);
             Assert.Equal("fanout", exchange.Type);
+            Assert.Equal(false, exchange.Durable);
+        }
+
+        [Fact]
+        public void BuildingDurableFanoutExchange()
+        {
+            const String name = "one_exchange";
+            var exchange = Exchange.DurableFanout(name);
+            Assert.Equal(name, exchange.Name);
+            Assert.Equal("fanout", exchange.Type);
+            Assert.Equal(true, exchange.Durable);
         }
 
         [Fact]
@@ -30,6 +52,17 @@ namespace Carrot.Tests
             var exchange = Exchange.Topic(name);
             Assert.Equal(name, exchange.Name);
             Assert.Equal("topic", exchange.Type);
+            Assert.Equal(false, exchange.Durable);
+        }
+
+        [Fact]
+        public void BuildingDurableTopicExchange()
+        {
+            const String name = "one_exchange";
+            var exchange = Exchange.DurableTopic(name);
+            Assert.Equal(name, exchange.Name);
+            Assert.Equal("topic", exchange.Type);
+            Assert.Equal(true, exchange.Durable);
         }
 
         [Fact]
@@ -39,6 +72,17 @@ namespace Carrot.Tests
             var exchange = Exchange.Headers(name);
             Assert.Equal(name, exchange.Name);
             Assert.Equal("headers", exchange.Type);
+            Assert.Equal(false, exchange.Durable);
+        }
+
+        [Fact]
+        public void BuildingDurableHeadersExchange()
+        {
+            const String name = "one_exchange";
+            var exchange = Exchange.DurableHeaders(name);
+            Assert.Equal(name, exchange.Name);
+            Assert.Equal("headers", exchange.Type);
+            Assert.Equal(true, exchange.Durable);
         }
 
         [Fact]
