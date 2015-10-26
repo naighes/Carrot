dbc7da99d252af514caba54063c621faa416d842
Nicola Baldi <nic.baldi@gmail.com>
2015-10-26 07:03:58 +0100

    sylecopping.

--- src/Carrot.BasicSample/FooConsumer.cs
+++ src/Carrot.BasicSample/FooConsumer.cs
@@ -4,14 +4,13 @@ using Carrot.Messages;
 
 namespace Carrot.BasicSample
 {
-    class FooConsumer : Consumer<Foo>
+    internal class FooConsumer : Consumer<Foo>
     {
         public override Task ConsumeAsync(ConsumedMessage<Foo> message)
         {
             return Task.Factory.StartNew(() =>
             {
-                Console.WriteLine("received '{0}'",
-                    message.Headers.MessageId);
+                Console.WriteLine("received '{0}'", message.Headers.MessageId);
             });
         }
     }
--- src/Carrot.BasicSample/Program.cs
+++ src/Carrot.BasicSample/Program.cs
@@ -4,19 +4,19 @@ using Carrot.Messages;
 
 namespace Carrot.BasicSample
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             var channel = AmqpChannel.New("amqp://guest:guest@localhost:5672/",
-                              new MessageBindingResolver(typeof(Foo).Assembly));
+                                          new MessageBindingResolver(typeof(Foo).Assembly));
             var exchange = Exchange.Direct("source_exchange");
 
             channel.Bind("my_test_queue", exchange)
                    .SubscribeByAtLeastOnce(_ => { _.Consumes(new FooConsumer()); });
 
             channel.PublishAsync(new OutboundMessage<Foo>(new Foo { Bar = 2 }),
-                     exchange);
+                                 exchange);
 
             Console.ReadLine();
 
